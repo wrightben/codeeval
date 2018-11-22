48636263f6f6940c576224f7e1e0b7f945fdaa57
wrightben <mail@wrightben.com>
2018-11-22 14:35:18 -0500

    gitall commit

---
+++ code/spiralPrinting.php
@@ -0,0 +1,164 @@
+#!/usr/bin/php
+<?php
+
+
+$file;
+(isset($argv[1])) ? $file = $argv[1]:exit("Must provide a file");
+ 
+/*  ArrayWalker (interface)
+ 
+*/
+interface ArrayWalker {
+    function walk();
+    function getElement($y,$x);
+}
+ 
+/* FileParser (interface)
+ 
+*/
+interface FileParser {
+    function toObjectFromFile($fileContentsAsArray);
+}
+ 
+/*  SpiralArray:
+    Expects 2D array; Walks array in specified pattern,
+    and returns elements given a position.
+*/
+class SpiralArray implements ArrayWalker,FileParser {  
+ 
+    private $twoDimArray;
+    private $twoDimArrayOrdered = array();
+ 
+    function __construct() {
+ 
+    }
+ 
+    function __toString() {
+        return "SpiralArray";
+    }
+ 
+    public function walk() {
+        $height = count($this->twoDimArray);
+        $width = count($this->twoDimArray[0]);
+        $box = $height * $width;
+ 
+        $this->right($height, $width, $box);
+ 
+        return $this;
+    }
+ 
+    public function getElement($y /*int*/,$x /*int*/) {
+        return $this->twoDimArray[$y][$x];
+    }
+ 
+    public function getElementAtPoint($i /*int*/) {
+        return $this->twoDimArrayOrdered[$i];
+    }
+ 
+    public function toObjectFromFile($fileContentsAsArray) {
+        $y = 0;
+        $x = 0;
+        foreach ($fileContentsAsArray as $line) {
+            $line = preg_split("/\s/",$line);
+            foreach ($line as $digit) {
+                $this->twoDimArray[$y][$x] = $digit;
+                $x ++;
+            }
+            $x = 0;
+            $y ++;
+        }
+        return $this;
+    }
+ 
+    public function get() {
+        return $this->twoDimArrayOrdered;
+    }
+ 
+    private function right ($height,$width,$box) {
+        $l = 0;
+        $r = $width - 1;
+        $t = 0;
+        $b = $height - 1;
+        $dir = 0; //1,2,3
+ 
+        $i1 = $t;
+        $i2 = $l;
+ 
+        for ($i = 0; $i < $box; $i ++ ){
+ 
+            array_push($this->twoDimArrayOrdered, $this->getElement($i1,$i2));
+ 
+             switch($dir) {
+             case 0:
+                $i2 += 1;
+                if ($i2 > $r) { //Going right
+                    $i2 -= 1; //Shift back left
+                    $t += 1; //Set the top boundary down
+                    $i1 = $t;
+                    $dir = 1; //Change direction
+                }
+                break;
+            case 1:
+                $i1 += 1;
+                if ($i1 > $b) { //Going down
+                    $i1 -= 1; //Shift back up
+                    $r -= 1; //Shift the right boundary left
+                    $i2 = $r;
+                    $dir = 2;
+                }
+                break;
+            case 2:
+                $i2 -= 1;
+                if ($i2 < $l) { //Going left
+                    $i2 += 1; //Shift back right
+                    $b -= 1; //Shift the bottom boundary up
+                    $i1 = $b;
+                    $dir = 3;
+                }
+                break;
+            case 3:
+                $i1 -= 1;
+                if ($i1 < $t) { //Going up
+                    $i1 += 1; //Shift back down
+                    $l += 1; //Shift the left boundary right
+                    $i2 = $l;
+                    $dir = 0;
+                }
+                break;
+            }
+        }
+    }
+ 
+    private function left() {
+        /* Future Implementation */
+    }
+ 
+}
+ 
+/*  FileHandler:
+    Wrap some common read operations.
+*/
+class FileHandler {
+ 
+    /*  readByLine
+        Use: FileHandler::readByLine('filePath',true);
+    */
+    public static function readByLine($file /*Full file path*/,$rtrim /*Boolean*/) { // Return an array of lines
+        $lines = array();
+        (ISSET($rtrim) && ($rtrim == true)) ? $rtrim = true: $rtrim = false;
+        $file_handle = fopen($file, "r");
+        while (!feof($file_handle)) {
+            $line = fgets($file_handle);
+            if ($rtrim) { $line = rtrim($line); }
+            array_push($lines,$line);
+        }
+        fclose($file_handle);
+        return $lines;
+    }
+}
+ 
+$a = new SpiralArray();
+print join(" ",$a->toObjectFromFile(FileHandler::readByLine($file,true))->walk()->get());
+ 
+exit;
+?>
