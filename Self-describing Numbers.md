fbda076e66ff489fb2812154922de9d52e458617
wrightben <mail@wrightben.com>
2018-11-22 16:19:02 -0500

    gitall commit

--- Self-describing Numbers.md
+++ Self-describing Numbers.md
@@ -19,13 +19,13 @@ If the number is a self-describing number, print out a 1. If not, print out a 0 
 > 1<br/>0<br/>1
 
 For the curious, here’s how 2020 is a self-describing number:<br />
-> **orderedHash**{ *"0"*:2, *"1"*:0, *"2"*:2, *"3"*:0 }
+> **orderedHash**[ 0:2 1:0 2:2 3:0 ]
 
 ## Code
 
-[selfDescribingNumbers.php](https://github.com/wrightben/codeeval/blob/master/code/selfDescribingNumbers.php)
+[selfDescribingNumbers.js](https://github.com/wrightben/codeeval/blob/master/code/selfDescribingNumbers.js)
 
 #### Run the script
 ```sh
-time php selfDescribingNumbers.php input_sample_4.txt
+time node selfDescribingNumbers.js input_sample_4.txt
 ```
---
+++ code/selfDescribingNumbers.js
@@ -0,0 +1,40 @@
+var	fs = require('fs'),
+	file = process.argv[2];
+	
+	
+var isSelfDescribing = function( number ) {
+
+	/* 	number = 1210
+		n = [1,2,1,0]
+		h = [_,_,_,_]
+		
+		Iteration 1: var i = 0, e = 1;
+		h[0(i)] = "_" (= 0)
+		h[1(e)] = _ (++ 1)
+	*/
+
+	var n = number.split(""),
+		h = new Array(n.length);
+	
+	n.forEach(function(e,i) { // element, iterator (0,1,...)
+		
+		if ( typeof h[i] == "undefined" ) { h[i] = 0; } // Use iterator to create zeros in h array.
+		
+		(h[e] == 1) ? h[e] += 1 : h[e] = 1; // Increment number count for symbolic int
+		
+	});
+	
+	
+	return ( n.join("") == h.join("") ) ? 1 : 0; // ("1210" == "1210") ? 1 : 0
+		
+	
+};
+
+
+require('fs')
+	.readFileSync( file , 'utf-8')
+	.split(/\r?\n/)
+	.forEach(function( word ) {
+		console.log( isSelfDescribing( word ) );
+	})
+
