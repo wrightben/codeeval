dc910f71fe6271fe26de9ebcbeb464fdcda1b435
wrightben <mail@wrightben.com>
2018-11-23 11:00:59 -0500

    gitall commit

--- code/arrayAbsurdity.php
+++ code/arrayAbsurdity.php
@@ -5,25 +5,27 @@ $file;
 ( isset( $argv[1] ) ) ? $file = $argv[1] : exit( "Must provide a file" );
 
 $contents = readfile($file);
+$contents = explode("\n", $contents);
 
-print $contents;
 
-/*
 foreach ($contents as $line) {
-    if (strlen($line) < 3) {continue;}
-    $segments = preg_split("/;/",$line);
-    $length = $segments[0];
-    $list = preg_split("/,/",$segments[1]);
-    $expected = 0;
-    $actual = 0;
-    for ($i = 0; $i < $length; $i++) {
-        $expected = $expected + $i;
-        $actual += $list[$i];
-    }
-    print $length - (abs($actual - $expected)+1)."\n";
-}
-*/
+	
+	if (strlen($line) < 3) { continue; }
+    
+	$segments = explode("/;/",$line);
+	$length = $segments[0];
+	$list = explode("/,/",$segments[1]);
+	$expected = 0;
+	$actual = 0;
 
+	for ($i = 0; $i < $length; $i++) {
+		$expected = $expected + $i;
+		$actual += $list[$i];
+	}
+	
+	print $length - (abs($actual - $expected)+1)."\n";
+
+}
 
 exit;
 ?>
