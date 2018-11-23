8c342b56261923675749b243d223187d6c060e48
wrightben <mail@wrightben.com>
2018-11-23 00:09:56 -0500

    gitall commit

--- code/robotMovements.js
+++ code/robotMovements.js
@@ -1,17 +1,126 @@
 var	fs = require('fs'),
 	file = process.argv[2];
+	
+	
+var selectMove = function( moves ) {
+	return moves[ Math.floor( Math.random() * moves.length ) ];
+}
+
+
+var setPos = function( pos, board ) {
+
+	board[ pos - 1 ] = 1;
+
+	return pos;
+}
+
+var isValidMove = function( move, pos, gridSize, board ) {
+	
+	var	next = pos; // Destination
+
+	if ( "u" == move ) {
+		next -= gridSize;
+		if ( next < 1  ) { return false; } // Bound
+	}
+	
+	if ( "d" == move ) {
+		next += gridSize;
+		if ( next > board.length ) { return false; } // Bound
+	}
+
+	if ( "l" == move ) {
+		next = pos - 1;
+		if ( (next % gridSize) == 0 ) { return false; } // Bound
+	}
+	
+	if ( "r" == move ) {
+		next = pos + 1;
+		if (pos % gridSize == 0) { return false; } // Bound
+	}	
+
+	if ( board[ next - 1 ] == 1 ) { return false; } // Already Visited
+
+	return next;
+}
+
+var walkPath = function( pos, gridSize, board ) {
+	
+	var	currentPath = [pos],
+		dir = [ 'u', 'd', 'l', 'r' ],
+		next;
+	
+	setPos( pos, board ); // Starting Position
+
+	while ( true ) {
+	
+		// Seek Moves
+		dir.next = [];
+		dir.forEach(function(e,i) {
+			var next = isValidMove( e, pos, gridSize, board );
+			if ( Boolean(next) == true ) { dir.next.push( next ); }
+		});
+		
+		// Path Blocked (No valid moves available)
+		if ( dir.next.length == 0 ) { return currentPath; }
+		
+		// Select Move
+		pos = setPos( selectMove( dir.next ), board );		
+		currentPath.push(pos);
+		
+		// Path Complete
+		if ( pos == board.length ) { return currentPath; }
+		
+	}
+
+	// void
+}
 
 
 var getPaths = function( gridSize, limit, debug ) {
 
+	var	board_length = Math.pow( gridSize, 2 ),
+		board,
+		paths = { "blocked":0, "length":0 },
+		currentPath;
 
+
+	while( Boolean( paths.blocked < limit ) ) {
+		
+		board = new Array( board_length );
+		currentPath = walkPath( 1, gridSize, board );
+		if ( debug == true ) { console.log(currentPath); }
+		( board[ board_length - 1 ] == 1 ) ? paths[ currentPath.join(",") ] = 1 : paths.blocked += 1;
+
+	}
+	
+	paths.length = Object.keys( paths ).length - 2;
+	return paths.length;
 
 };
 
 
-require('fs')
-	.readFileSync( file , 'utf-8')
-	.split(/\r?\n/)
-	.forEach(function( line ) {
-		console.log( getPaths( 4, 1500, true ) );
-	});
+console.log( getPaths( 5, 100500, false ) );
+
+
+
+
+/*
+
+	Movements:
+	
+		Up/Down: 	-/+ gridSize
+		Left/Right:	-/+ 1
+	
+	Boundaries: (pos: current position; 1-based)
+	
+		Top:	(pos - gridSize) < 1
+		Bottom: (pos + gridSize) > Math.pow(gridSize, 2) // Or board.length
+		Left:	pos - 1 % gridSize = 0
+		Right:	pos % gridSize = 0
+		
+	Invalid Moves:
+		
+		* Off grid
+		* Visited
+
+*/
