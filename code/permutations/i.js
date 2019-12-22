c5c574af64b15f6a1e31a302f645fc9a8a779a51
Benjamin Wright <wrightben@Martas-MacBook-Pro.local>
2019-12-22 02:24:32 -0500

    gitall commit

--- code/permutations/i.js
+++ code/permutations/i.js
@@ -18,23 +18,6 @@ var getNPR = function (n, r) {
 	
 	return _(n) / _(n - r);
 	
-}
-
-var getRandomID = function( o, n ) {
-	var _i = 0;
-	var _id = 0;
-	if (typeof n == "undefined") { n = 1; _i = n; }
-	
-	for (i in o) {
-		if ( o.hasOwnProperty(i) && (i != "length") && ( _i >= n ) ) {
-
-			_id = i;
-			break;
-
-		}
-		_i++;
-	}
-	return _id;
 }	
 
 
@@ -68,7 +51,7 @@ var sub = function(_array, m, n) {
 /*******************
 VARIABLES
 *******************/
-var 	n = 8;
+var 	n = 11;
 var	_iph = {}; // Hash lookup { n : 1 }
 var	_ip = []; // [] (int): Sortable integers
 var	_nph = { "length": 0 }; // Collection: { "length" :0, "n" : [ [](int), n ] }
@@ -79,7 +62,6 @@ for (var i = 0; i < n; i ++) { x.push(i+1); }
 x = [ x, Number(x.join("")) ];
 
 
-console.log(x);
 
 _nph[ x[1] ] = [x, (new Array(n+1)).fill(0) ];
 _nph.length += 1;
@@ -87,43 +69,69 @@ _nph.length += 1;
 // console.log(getRandomID(_nph));
 
 while ( _nph.length > 0 ) {
-	
-	_id = getRandomID(_nph,0);
-	_element = _nph[ _id ][0];
-	_status =  _nph[ _id ][1];
+
+	var _ids = [];
+	for (p in _nph) { // Max 10 ids
+		if ( _nph.hasOwnProperty(p) && (p != "length") ) {
+			_id = p;			
+			_ids.push( _id );
+		}
+	}
 
 	
 	for (var i = n + 1; i > 0; i-- ) { // 6, 5, 4, 3, 2, 1
-
-		if ( _status[i-1] != 1 ) { // Mirrored value: Skip
 	
-			var ro = sub(_element,i,n); // Args: [[1,2,3,4,5,6], 123456 ], i, 4
-			var _roID = ro[1];
+		_ids.forEach(function(e, f) {
+
+			_id = e;
+			_element = _nph[ _id ][0];
+			_status =  _nph[ _id ][1];
+
+	
+			if ( _status[i-1] != 1 ) { // Mirrored value: Skip
+	
+				var ro = sub(_element,i,n); // Args: [[1,2,3,4,5,6], 123456 ], i, 4
+				var _roID = ro[1];
 			
-			if ( 	(typeof _iph[_roID] == "undefined" ) && // Not _ip
-				(typeof _nph[_roID] == "undefined") ) 	// Not _np
-			{
-				_nph[_roID] = [ro, (new Array(n+1)).fill(0) ]; // _nph.ID = [ element, status ]
-				_nph[_roID][1][i - 1] = 1; // Mirrored Value: ro.status[..i..] = 1;
-				_nph.length += 1;
-			} else if ( typeof _iph[ro[1]] == "undefined" ) {
-				// Not completed, but mirrored
-				_nph[_roID][1][i - 1] = 1; // Mirrored Value: ro.status[..i..] = 1;
+				if ( 	(typeof _iph[_roID] == "undefined" ) && // Not _ip
+					(typeof _nph[_roID] == "undefined") ) 	// Not _np
+				{
+					_nph[_roID] = [ro, (new Array(n+1)).fill(0) ]; // _nph.ID = [ element, status ]
+					_nph[_roID][1][i - 1] = 1; // Mirrored Value: ro.status[..i..] = 1;
+					_nph.length += 1;
+				} else if ( typeof _iph[ro[1]] == "undefined" ) {
+					// Not completed, but mirrored
+					_nph[_roID][1][i - 1] = 1; // Mirrored Value: ro.status[..i..] = 1;
+				}
+			
+				_status[i-1] = 1; // _element.status[..i..] = 1;
+			
 			}
-			
-			_status[i-1] = 1; // _element.status[..i..] = 1;
-			
-		}
+
+		});	
+	
+
+
 		
 	}
 	
+	_ids.forEach(function(e, i) {
+		
+		_id = e;
+		_element = _nph[ _id ][0];
+		_status =  _nph[ _id ][1];
+		
+		 // REM from _nph
+		delete _nph[ _id ]; _nph.length -= 1;
 	
- 	// REM from _nph
- 	delete _nph[ _id ]; _nph.length -= 1;
- 	
- 	// ADD to _iph, _ip
- 	_iph[_element[1]] = 1; // iph[ Number ] = 1
-	_ip.push(_element[1]); // _ip.push( Number )
+		// ADD to _iph, _ip
+		if (typeof _iph[_id] == "undefined" ) {
+			_iph[_id] = 1; // iph[ Number ] = 1
+			_ip.push(_id); // _ip.push( Number )
+		}
+	
+	});
+
 
 		
 }
