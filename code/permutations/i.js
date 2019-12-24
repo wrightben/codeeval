38c7622ceb40c836ecea43fb9ec5f2435c8ffa63
Benjamin Wright <mail@wrightben.com>
2019-12-24 01:15:23 -0500

    gitall commit

--- code/permutations/i.js
+++ code/permutations/i.js
@@ -71,7 +71,7 @@ var subtract = function(s, m, n) {
 
 
 // VARIABLES
-var 	n = 8;
+var 	n = 7;
 var	f = getNPR(n);
 var	_iph = { "length": 0 }; // Hash lookup { n : 1 }
 var	_nph = { "length": 0 }; // Collection: { "length" :0, "n" : [ [](int), n ] }
