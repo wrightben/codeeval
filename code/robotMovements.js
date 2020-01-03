00054cc70d6fb5f86cfbeeb39f720b65daef9f05
wrightben <mail@wrightben.com>
2020-01-03 10:18:27 -0500

    gitall commit

--- code/robotMovements.js
+++ code/robotMovements.js
@@ -9,7 +9,8 @@ var	size = 4, // Grid
 	path = [1], // Initial
 	moves = [2, 5], // R, L, U, D; pos += [1, -1, size^2, -size^2]    
 	paths = { "c":0, "u":0 },
-	randomGuesses = 5500;  // Wrapper program finds limit by increasing this variable and/or joining sets
+	randomGuesses = 4500;	// Wrapper program finds limit by increasing 
+				// randomGuesses (setting high-water mark) and/or joining sets
 
 for (var i = 0; i < randomGuesses ; i++) {
 
